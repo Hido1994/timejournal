1436e44ffd5b746772cd739c461a26d44dc47233
David Hinterndorfer <david.hinterndorfer@gmail.com>
2025-01-14 19:39:51 +0100

    fixed not running version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "at.dhinterndorfer.timejournal"
+    namespace = "com.example.timejournal"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "at.dhinterndorfer.timejournal"
+        applicationId = "com.example.timejournal"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/at/dhinterndorfer/timejournal/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package at.dhinterndorfer.timejournal
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
